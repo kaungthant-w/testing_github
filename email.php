ce4ceea4e97e3f8793863eac050064afc039899b
kaungthant-w <109889307+kaungthant-w@users.noreply.github.com>
2022-07-24 23:18:12 -0800

    Update email.php

--- email.php
+++ email.php
@@ -1,8 +1,2 @@
-echo "creating email file";
-ech "another email send";
-
-echo "third line from cli";
-echo "third line from online";
-
-echo "end line from online";
-echo "end line from cli";
+echo "sending email";
+echo "another email sent";
